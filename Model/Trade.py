a9eea7f2e3b8614ca1865c434b0f99f1036b4c98
Richard Staines <richard.staines@livce.com>
2023-10-27 10:00:34 +0100

    Load the data into the DB + treat cash distribution as a Div not a trade.

--- Model/Trade.py
+++ Model/Trade.py
@@ -17,3 +17,11 @@ class Trade(Base):
     settle_date = Column(DateTime())
     created_on = Column(DateTime(), default=datetime.now())
     updated_on = Column(DateTime(), default=datetime.now(), onupdate=datetime.now)
+
+    def __repr__(self):
+        return (f"Trade {self.id} instrument={self.instrument} sedol={self.sedol} "
+                f"instrument_description={self.instrument_description} "
+                f"buy_sell={self.buy_sell} quantity={self.quantity} price={self.price} "
+                f"net_consideration={self.net_consideration} "
+                f"trade_date={self.trade_date} settle_date={self.settle_date} "
+                f"created_on={self.created_on} updated_on={self.updated_on}\n")
