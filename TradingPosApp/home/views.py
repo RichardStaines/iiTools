04c2525bcc1e184fbede24d14f53f969027bbf4f
Richard Staines <richard.staines@livce.com>
2023-11-10 11:03:11 +0000

    Django project been moved to its own repo.

--- TradingPosApp/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
-from django.views.generic import TemplateView
-
-# Create your views here.
-def home(request):
-    return HttpResponse('Hello World')
